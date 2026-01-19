b3863cfa13a289f79e041234badf14e736e40a98
Swan-le <134010793+Swan-le@users.noreply.github.com>
2026-01-19 15:44:50 -0600

    Updated Portscan to fix indent & added common ports

--- Portscanner/portscanner.py
+++ Portscanner/portscanner.py
@@ -1,52 +1,40 @@
-import socket 
+import socket
 from datetime import datetime
 
 def scan_port(target_host, port):
-    """
-    Attempt to connect to a single port.
-    Returns True if port is open (Connection Successful).
-    """
     try:
-        #Create a TCP/IP Socket (AF_INET = IPv4, SOCK_STREAM = TCP
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        #Set a short timeout so we don't hang on a closed port
-        sock.settimeout(1.0) #1 Second
-
-        #Try to connect > Returns 0 on sucess, error code if failed
+        sock.settimeout(1.0)
+        
         result = sock.connect_ex((target_host, port))
 
-        sock.close () # Makes sure the sock is closed
-
-        return result == 0 #0 means port was sucessful
+        sock.close()
+        
+        return result == 0 
     except Exception:
         return False
 
-    def main ():
-        # ***CONFIG***
-        target = input ("Enter Taret (IP or hostname, Example: 192.168.1.1 or scanme.nmap.org): ").strip()
-        start_port = int(input("start port (Example: 1): "))
-        end_port = int(input("End port (Example: 1024): " ))
+def main():
+    target = input("Enter Target IP or hostname. Example: 192.168.1.1: ").strip()
+    common_ports = [21, 22, 23, 25, 53, 67, 68,  80, 110, 123, 135, 139, 143, 161, 162, 389, 443, 445, 514, 636, 993, 995, 1433, 1521, 1723, 2049, 3306, 3389, 5432, 5900, 6379, 8000, 8080, 8888, 8443, 9000, 1883, 8883, 873, 1750]
 
-        print(f"\nScanning {target} from port {start_port} through {end_port}...")
-        print(f"\nStarted at: {datetime.now().strftime('%Y-%m-%d %H:%M')}\n")
-
-        open_ports = []
-        
-        for port in range (start_port, end_port + 1):
-            if scan_port(target, port):
-                print(f"port {Port:>5} > OPEN")
-                open_ports.append(port)
-            else:
-                # print(f"Port {Port:>5} > CLOSED")
-                pass
-
-        print(f"\nScan finished at: {datetime.now().strftime('%Y-%m-%d %H:%M')}\n")
-         if open_ports:
-          print(f"Open Ports Found {open_ports}")
-         else:
-        print(f"No Open ports found in the scan range.")
-         if __name__ == "__main__":
-main()
-
-
+    print(f"\nScanning {target}, common ports: {common_ports} only")
+    print(f"Started at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+    
+    open_ports = []
+    
+    for port in common_ports:
+        if scan_port(target, port):
+            print(f"Port {port:>5} is OPEN")
+            open_ports.append(port)
+        else:
+            print(f"Port {port:>5} is CLOSED")
+            pass
+    print(f"\nScan finished at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    if open_ports:
+        print(f"Open ports found: {open_ports}")
+    else:
+        print("No open ports found on target")
+if __name__ == "__main__":
+    main()
